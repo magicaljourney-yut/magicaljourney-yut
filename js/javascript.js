14e108aff0a427d4af6739bab58644253ff3da54
magicaljourney-yut <95560208+magicaljourney-yut@users.noreply.github.com>
2021-12-05 15:49:31 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+$(function () {
+  var $demo1 = $('.gallery');   //コンテナとなる要素を指定
+
+  $demo1.imagesLoaded(function(){ //imagesLoadedを使用し、画像が読み込みまれた段階でMasonryの関数を実行させる
+    //Masonryの関数↓
+    $demo1.masonry({              //オプション指定箇所
+      itemSelector: '.item',   //コンテンツを指定
+      columnWidth: 40,           //カラム幅を設定
+      fitWidth: true,             //コンテンツ数に合わせ親の幅を自動調整
+      gutter: 20
+    });
+  });
+});
